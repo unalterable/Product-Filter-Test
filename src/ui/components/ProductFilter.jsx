246b5b9c3174ce825c66968fac3e84f676b88bf0
Oliver Diamond <diamond.oliver@gmail.com>
2019-03-03 17:05:17 +0000

    Filter

--- src/ui/components/ProductFilter.jsx
+++ src/ui/components/ProductFilter.jsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useState }from 'react';
 
 import { withStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
 
 const styles = () => ({
   container: {
     display: 'flex',
+  },
+  formControl: {
+    width: '140px',
   },
   filterer: {
     flex: '25%',
@@ -20,11 +27,23 @@ const styles = () => ({
 });
 
 const ProductFilter = ({ classes }) => {
+  const [ productType, setProductType ] = useState('');
+
+
   return (
     <div className={classes.container}>
+
       <Paper className={classes.filterer}>
-        Filter
+        <FormControl className={classes.formControl}>
+          <InputLabel>Product Type</InputLabel>
+          <Select value={productType} onChange={e => setProductType(e.target.value)} >
+            <MenuItem value={'vegetables'}>Vegetables</MenuItem>
+            <MenuItem value={'dairy'}>Dairy</MenuItem>
+            <MenuItem value={'meat'}>Meat</MenuItem>
+          </Select>
+        </FormControl>
       </Paper>
+
       <Paper className={classes.itemsList}>
         List
       </Paper>
